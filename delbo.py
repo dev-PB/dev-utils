3f455da4e12aa32ae54b3945c922f3b4bc5f3779
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-03-29 16:00:23 +0000

    Double check if running delbo in correct dir

--- delbo.py
+++ delbo.py
@@ -3,6 +3,14 @@ import os, shutil
 CLI_RED = '\033[31m'
 CLI_END = '\033[0m'
 
+EXTENSIONS = (".sln", ".csproj", ".vs", ".iml", ".idea")
+if not any(item.endswith(EXTENSIONS) for item in os.listdir(".")):
+    print("The current directory doesn't appear to be the root of a solution or project. Type 'delbo' to continue.")
+
+    if (input(">").lower() != "delbo"):
+        print("Cancelled.")
+        quit()
+
 for path, folders, files in os.walk("."):
     for folder in folders:
         if folder == "node_modules":
